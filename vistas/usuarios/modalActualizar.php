1b016ea9a7a7fa61ee1f293179072bda9aab8d97
Alvarez20-end <kevinkac123@gmail.com>
2026-03-15 20:45:26 -0600

    actualizar con datos funcionando

--- vistas/usuarios/modalActualizar.php
+++ vistas/usuarios/modalActualizar.php
@@ -1,30 +1,34 @@
 <!-- Modal -->
-<form id="frmActrualizarUsuario" method="POST" onsubmit="return actualizarUsuario()">
+<form id="frmActualizarUsuario" method="POST" onsubmit="return actualizarUsuario()">
 <div class="modal fade" id="modalActualizarUsuarios" tabindex="-1" role="dialog" aria-labelledby="exampleModalLabel" aria-hidden="true">
 <div class="modal-dialog modal-lg" role="document">
 <div class="modal-content">
 
     <div class="modal-header">
-        <h5 class="modal-title" id="exampleModalLabel">Actualizar usuariou</h5>
-        <button type="button" class="close" data-dismiss="modal" aria-label="Close" onclick="$('#modalAgregarUsuarios').modal('hide')">
-            <span aria-hidden="true">&times;</span>
+        <h5 class="modal-title">Actualizar usuario</h5>
+        <button type="button" class="close" data-dismiss="modal" onclick="$('#modalActualizarUsuarios').modal('hide')">
+            <span>&times;</span>
         </button>
     </div>
 
     <div class="modal-body">
 
+        <input type="text" id="idUsuario" name="idUsuario" hidden>
+
         <div class="row">
 
             <div class="col-sm-4">
-                <label for="paternou">Apellido paternou</label>
+                <label>Apellido paterno</label>
                 <input type="text" class="form-control" id="paternou" name="paternou" required>
             </div>
+
             <div class="col-sm-4">
-                <label for="maternou">Apellido maternou</label>
+                <label>Apellido materno</label>
                 <input type="text" class="form-control" id="maternou" name="maternou" required>
             </div>
+
             <div class="col-sm-4">
-                <label for="nombreu">nombreu</label>
+                <label>Nombre</label>
                 <input type="text" class="form-control" id="nombreu" name="nombreu" required>
             </div>
 
@@ -33,19 +37,21 @@
         <div class="row">
 
             <div class="col-sm-4">
-                <label for="fechaNacimientou">fecha de nacimiento</label>
+                <label>Fecha de nacimiento</label>
                 <input type="date" class="form-control" id="fechaNacimientou" name="fechaNacimientou">
             </div>
+
             <div class="col-sm-4">
-                <label for="sexou">sexou</label>
+                <label>Sexo</label>
                 <select class="form-control" id="sexou" name="sexou" required>
                     <option value=""></option>
-                    <option value="F">femenino</option>
-                    <option value="M">masculino</option>
+                    <option value="F">Femenino</option>
+                    <option value="M">Masculino</option>
                 </select>
             </div>
+
             <div class="col-sm-4">
-                <label for="telefonou">telefonou</label>
+                <label>Telefono</label>
                 <input type="text" class="form-control" id="telefonou" name="telefonou">
             </div>
 
@@ -54,35 +60,41 @@
         <div class="row">
 
             <div class="col-sm-4">
-                <label for="correou">correou</label>
+                <label>Correo</label>
                 <input type="mail" class="form-control" id="correou" name="correou">
             </div>
+
             <div class="col-sm-4">
-                <label for="usuariou">usuariou</label>
+                <label>Usuario</label>
                 <input type="text" class="form-control" id="usuariou" name="usuariou">
             </div>
+
             <div class="col-sm-4">
-                <label for="password">password</label>
+                <label>Password</label>
                 <input type="password" class="form-control" id="password" name="password">
             </div>
 
         </div>
 
         <div class="row">
+
             <div class="col-sm-12">
-                <label for="idRolu">Rol de usuariou</label>
+                <label>Rol de usuario</label>
                 <select name="idRolu" id="idRolu" class="form-control">
                     <option value="1">Cliente</option>
                     <option value="2">Administrador</option>
                 </select>
             </div>
+
         </div>
 
         <div class="row">
+
             <div class="col-sm-12">
-                <label for="ubicacionu">Ubicacion</label>
+                <label>Ubicación</label>
                 <textarea name="ubicacionu" id="ubicacionu" class="form-control"></textarea>
             </div>
+
         </div>
 
     </div>
